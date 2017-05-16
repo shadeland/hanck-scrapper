89087f9930c3a5ed6e05cb7d107ede1683e5e2d9
Adel Shahrezaei <shadel.shsh@gmail.com>
2017-05-16 14:28:07 -0400

    some git stuff, aded git ignore, FIXES  #1

--- test/index.js
+++ test/index.js
@@ -1,5 +1,39 @@
 /**
  * Created by adel on 5/15/17.
  */
+'use strict';
 
+const chai = require('chai')
+const expect = chai.expect;
+const path = require('path');
+const scrapper = require(path.join('..','lib','scrapper.js'))
 
+describe('Scrapper', function(){
+
+    describe("creation", function(){
+
+      it('should create a new scrcapper without config should fail', function () {
+
+        let scrp = new scrapper();
+
+        expect(scrp).to.be(undefined);
+
+      });
+
+      it('should create a new scrcapper object using provided config', function () {
+
+          let scrp = new scrapper({
+              name:'scap1',
+              q:'%40JohnHancockUSA',
+              count: 3,
+              type:'tw',
+              lastrunStart:'',
+              lastrunEnd:''});
+
+          expect(scrp.getName()).to.equal('scap1');
+
+        });
+
+    });
+
+});
